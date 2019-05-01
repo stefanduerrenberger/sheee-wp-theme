7dbf7fcdff4e17657f48d07ad31b6330ab1d62dc
Stefan Dürrenberger <sd@pm7.ch>
2019-05-01 15:36:31 +0200

    Header logo linked

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 				?>
             </div>
         </nav><!-- #site-navigation -->
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Sheee-Logo-compressed.svg" alt="Sheee Logo" id="sheeeLogo">
+        <a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Sheee-Logo-compressed.svg" alt="Sheee Logo" id="sheeeLogo"></a>
     </header><!-- #masthead -->
 
     <div id="content" class="site-content">
