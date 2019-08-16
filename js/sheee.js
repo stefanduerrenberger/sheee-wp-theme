d6362318012294cfc459f245541d0ed7921f0d20
Stefan Dürrenberger <sd@pm7.ch>
2019-08-16 07:45:52 +0200

    Added swipe to carousel

--- js/sheee.js
+++ js/sheee.js
@@ -4,12 +4,25 @@ jQuery(document).ready(function () {
         jQuery(event.target).find(".amazingaudioplayer-time").show();
 
         // stop all other players on the page
-        jQuery(".wonderpluginaudio").each(function() {
+        jQuery(".wonderpluginaudio").each(function () {
             if (this != event.target)
-            jQuery(this).data("object").stopAudio();
+                jQuery(this).data("object").stopAudio();
         });
     });
     jQuery(".player-column .wonderpluginaudio").bind("amazingaudioplayer.stopped", function (event, index) {
         jQuery(event.target).find(".amazingaudioplayer-time").hide();
+    });
+
+    // Testimonials
+    /*
+    jQuery('#content .easy-t-slideshow-wrap .cycle-slideshow::before').on('click', function () {
+        jQuery('.cycle-slideshow').gp_cycle('next');
+        console.log('click prev');
+    });
+    */
+
+    // Enable Swipe
+    jQuery('.cycle-slideshow').gp_cycle({
+        swipe: true
     });
 });
